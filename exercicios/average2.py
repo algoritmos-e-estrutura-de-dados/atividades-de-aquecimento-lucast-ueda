b827bccb13919b640fe1d7440302551fc3046ef9
lucas.ueda@edu.unifil.br <labinfo@unifil.br>
2022-08-15 22:19:50 -0300

    exercicios

--- exercicios/average2.py
+++ exercicios/average2.py
@@ -4,6 +4,6 @@ c = float(input())
 a = (a*2)
 b = (b*3)
 c = (c*5)
-
+ 
 media = ((a+b+c)/10)
 print(f"MEDIA = {media}")
--- exercicios/salario.py
+++ exercicios/salario.py
@@ -3,5 +3,5 @@ hora = int(input())
 recebe = float(input())
 
 salario = (hora*recebe)
-print(f"NUMBER = {n}")
+print(f"NUMBER = {n}") 
 print("SALARIO = U$ " "%.2f"%salario)
