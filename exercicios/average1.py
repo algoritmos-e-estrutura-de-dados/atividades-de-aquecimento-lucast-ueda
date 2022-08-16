5f767d87cecd7d3a695f886234c3316fc7e27244
lucas.ueda@edu.unifil.br <labinfo@unifil.br>
2022-08-15 21:23:15 -0300

    average1

--- exercicios/average1.py
+++ exercicios/average1.py
@@ -1,6 +1,6 @@
-a = float("%.1f" % input())
-b = float("%.1f" % input())
-a = a*3.5
-b = b*7.5
-media((a+b)/11)
-print("MEDIA = %.5f" % media)
+a = float(input())
+b = float(input())
+a = (a*3.5)
+b = (b*7.5)
+media = ((a+b)/11)
+print("MEDIA = %0.5f"%media)
